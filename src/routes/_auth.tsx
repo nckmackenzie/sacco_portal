083c3fec9a96eb3c370d40704e68ce80ce406439
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-07 13:28:50 +0300

    feat: add ability to toggle theme

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -1,10 +1,12 @@
 import {
+  Link,
   Outlet,
   createFileRoute,
   useLocation,
   useNavigate,
 } from '@tanstack/react-router'
 import { useEffect } from 'react'
+import { Bell, MoonIcon, SunIcon } from 'lucide-react'
 
 import {
   SidebarInset,
@@ -13,7 +15,9 @@ import {
 } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/ui/app-sidebar'
 import { useAuth } from '@/hooks/use-auth'
-import { Separator } from '@/components/ui/separator'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { useTheme } from '@/components/providers/theme-provider'
 
 const getPageTitle = (location: string) => {
   switch (location) {
@@ -43,7 +47,7 @@ export const Route = createFileRoute('/_auth')({
 function RouteComponent() {
   const { isAuthenticated, isLoading } = useAuth()
   const slocation = useLocation()
-  // const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const { theme, setTheme } = useTheme()
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -60,14 +64,42 @@ function RouteComponent() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 border-b items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
+        <header className="flex h-16 shrink-0 border-b items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 px-4 md:px-6">
+          <div className="flex items-center gap-2 ">
             <SidebarTrigger className="-ml-1 block md:hidden" />
-            <Separator orientation="vertical" className="h-4 mr-2" />
+
             <h2>{getPageTitle(slocation.pathname)}</h2>
           </div>
+          <div className="ml-auto flex items-center gap-2">
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            >
+              {theme === 'light' ? (
+                <SunIcon className="h-4 w-4" />
+              ) : (
+                <MoonIcon className="h-4 w-4" />
+              )}
+            </Button>
+            <div
+              className={cn(
+                buttonVariants({ variant: 'outline', size: 'icon' }),
+                'relative',
+              )}
+              role="button"
+            >
+              <Link
+                to="/notifications"
+                className="text-gray-500 hover:text-gray-700"
+              >
+                <Bell size={20} />
+                <span className="absolute top-0 right-0 h-2 w-2 bg-red-500 rounded-full"></span>
+              </Link>
+            </div>
+          </div>
         </header>
-        <div className="flex flex-col flex-1 gap-4 p-4 ">
+        <div className="flex flex-col flex-1 gap-4 p-4 md:p-6 ">
           <Outlet />
         </div>
       </SidebarInset>
