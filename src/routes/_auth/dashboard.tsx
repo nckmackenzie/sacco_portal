17303cc6c7f0f3694ca87e17595f82b198006f9c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-12 19:26:00 +0300

    feat: add loan stats

--- src/routes/_auth/dashboard.tsx
+++ src/routes/_auth/dashboard.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { useDocumentTitle } from '@/hooks/use-title'
 
@@ -6,6 +6,16 @@ import { ErrorComponent } from '@/components/custom/error'
 import { dashboardQueryOptions } from '@/features/dashboard/services/query-options'
 import { DashboardStats } from '@/features/dashboard/components/dashboard-stats'
 import { RecentTransactions } from '@/features/dashboard/components/recent-transactions'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { formatCurrency, formatDate, getLoanStatus } from '@/lib/formatters'
+import { Progress } from '@/components/ui/progress'
+import { LoanStatus } from '@/features/dashboard/components/loan-status'
 
 export const Route = createFileRoute('/_auth/dashboard')({
   loader: ({ context: { queryClient } }) =>
@@ -26,10 +36,108 @@ function RouteComponent() {
       <DashboardStats data={data.stats} />
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <RecentTransactions data={data.transactions.splice(0, 5)} />
-        <div className="col-span-1 lg:col-span-1">
-          <h2 className="text-lg font-semibold">Notifications</h2>
-          {/* Add your notifications component here */}
-        </div>
+        <Card className="shadow-none col-span-1 lg:col-span-2 self-start">
+          <CardHeader className="flex flex-row items-center justify-between ">
+            <div>
+              <CardTitle className="text-lg font-semibold ">
+                Active / Pending Loan(s)
+              </CardTitle>
+              <CardDescription className="text-sm text-muted-foreground">
+                Your active loans or pending applications.
+              </CardDescription>
+            </div>
+            {data.loans.length > 0 && (
+              <Link
+                to="/transactions"
+                className="text-link text-xs font-semibold"
+              >
+                View All
+              </Link>
+            )}
+          </CardHeader>
+          <CardContent className="space-y-4">
+            {data.loans.length > 0 ? (
+              data.loans.map(
+                ({
+                  id,
+                  loanType: { name },
+                  loanAmount,
+                  loanBalance,
+                  applicationDate,
+                  nextDueDate,
+                  purpose,
+                  loanStatus,
+                  completedAt,
+                  writtenOff,
+                }) => {
+                  const loanProgress = Math.round(
+                    ((loanAmount - loanBalance) / loanAmount) * 100,
+                  )
+                  return (
+                    <div key={id} className="rounded-md border p-4">
+                      <div className="mb-2 flex items-center justify-between">
+                        <div className="flex items-center gap-2">
+                          <div>
+                            <h4 className="font-medium">{name}</h4>
+                            <p className="text-xs text-muted-foreground capitalize">
+                              {purpose}
+                            </p>
+                          </div>
+                          <LoanStatus
+                            status={getLoanStatus(
+                              loanStatus,
+                              completedAt,
+                              nextDueDate,
+                              loanBalance,
+                              writtenOff,
+                            )}
+                          />
+                        </div>
+                        <div className="text-right">
+                          <p className="font-medium">
+                            {formatCurrency(loanAmount)}
+                          </p>
+                          {loanStatus === 'approved' && (
+                            <p className="text-xs text-muted-foreground">
+                              Remaining: {formatCurrency(loanBalance)}
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                      <div className="space-y-1">
+                        <div className="flex justify-between text-xs">
+                          <span>Repayment Progress</span>
+                          <span>{loanProgress}%</span>
+                        </div>
+                        <Progress value={loanProgress} className="h-2" />
+                      </div>
+                      <div className="mt-2 grid grid-cols-2 gap-2 text-xs">
+                        <div>
+                          <span className="text-muted-foreground">
+                            Application Date:{' '}
+                          </span>
+                          <span>{formatDate(applicationDate)}</span>
+                        </div>
+                        {nextDueDate && (
+                          <div className="text-right">
+                            <span className="text-muted-foreground">
+                              Due Date:{' '}
+                            </span>
+                            <span>{formatDate(nextDueDate)}</span>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  )
+                },
+              )
+            ) : (
+              <p className="text-base text-muted-foreground mt-1 text-center">
+                No active loans available.
+              </p>
+            )}
+          </CardContent>
+        </Card>
       </div>
 
       {/* <FullPageLoader message="Fetching your data..." /> */}
