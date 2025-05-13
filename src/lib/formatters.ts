5039a6fcdcdfccbcfd8a9b19a181086efc253c58
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-13 21:35:18 +0300

    chore: formatter for date to yy-mm-dd

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -44,6 +44,16 @@ export function formatDate(date: string | Date) {
   })
 }
 
+export function formatDateToYMD(date: string | Date): string {
+  const d = new Date(date)
+
+  const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
+  const month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d)
+  const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d)
+
+  return `${year}-${month}-${day}`
+}
+
 interface ApiErrors {
   [key: string]: string
 }
