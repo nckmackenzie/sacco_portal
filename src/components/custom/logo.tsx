3efdbc8a39dbcf6ac215357c3ffb3ade3f099fb9
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-15 13:23:58 +0300

    bug fixes

--- src/components/custom/logo.tsx
+++ src/components/custom/logo.tsx
@@ -1,4 +1,4 @@
-import type {SVGAttributes} from 'react';
+import type { SVGAttributes } from 'react'
 import { cn } from '@/lib/utils'
 
 interface AppLogoProps extends React.ComponentProps<'div'> {
--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -338,9 +338,7 @@ function getPayloadConfigFromPayload(
     ] as string
   }
 
-  return configLabelKey in config
-    ? config[configLabelKey]
-    : config[key]
+  return configLabelKey in config ? config[configLabelKey] : config[key]
 }
 
 export {
