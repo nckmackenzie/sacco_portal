303d632ee94206bb8b26ba0f0f274d5f1ecb4415
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-16 20:12:11 +0400

    style: add option to pass indicator color

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -3,11 +3,17 @@ import * as ProgressPrimitive from '@radix-ui/react-progress'
 
 import { cn } from '@/lib/utils'
 
+interface ProgressProps
+  extends React.ComponentProps<typeof ProgressPrimitive.Root> {
+  indicatorBackground?: string
+}
+
 function Progress({
   className,
   value,
+  indicatorBackground,
   ...props
-}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+}: ProgressProps) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
@@ -19,7 +25,10 @@ function Progress({
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-primary h-full w-full flex-1 transition-all"
+        className={cn(
+          'bg-primary h-full w-full flex-1 transition-all',
+          indicatorBackground,
+        )}
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
