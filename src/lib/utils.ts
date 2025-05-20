9ecbf89e8f62b79b9f704ffc5c55bc7a1dcde67f
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-20 16:49:36 +0300

    chore: add generateUniqueString utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,14 @@ import type { ClassValue } from 'clsx'
 export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs))
 }
+
+export function generateUniqueString(length: number) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
+}
