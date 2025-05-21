2e4a09a2a814da2a2146e148cd1f519e6f66e42c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-21 12:10:48 +0300

    chore: schema for loan application

--- src/features/loans/utils/loan-schema.ts
+++ src/features/loans/utils/loan-schema.ts
@@ -1,23 +1,54 @@
 import { z } from 'zod'
 import {
-  optionalNumberSchemaEntry,
   requiredNumberSchemaEntry,
   requiredStringSchemaEntry,
 } from '@/lib/schema-entries'
 
-export const loanFormSchema = z.object({
-  loanType: requiredStringSchemaEntry('Select Loan type.'),
-  loanAmount: requiredNumberSchemaEntry('Enter Loan amount.'),
-  purpose: requiredStringSchemaEntry('Enter Loan purpose.'),
-  repaymentPeriod: requiredNumberSchemaEntry('Enter Loan repayment .'),
-  loanLimit: optionalNumberSchemaEntry(),
-  canSelfGuarantee: z.boolean(),
-  guarantors: z
-    .object({
-      guarantorId: requiredStringSchemaEntry('Enter Guarantor ID.'),
-      amountGuaranteed: requiredNumberSchemaEntry(
-        'Enter amount guaranteed by the Guarantor.',
-      ),
-    })
-    .optional(),
-})
+export const loanFormSchema = z
+  .object({
+    loanType: requiredStringSchemaEntry('Select Loan type.'),
+    loanAmount: requiredNumberSchemaEntry('Enter Loan amount.'),
+    purpose: requiredStringSchemaEntry('Enter Loan purpose.'),
+    repaymentPeriod: requiredNumberSchemaEntry('Enter Loan repayment .'),
+    maximumRepaymentPeriod: requiredNumberSchemaEntry(),
+    loanLimit: requiredNumberSchemaEntry(),
+    canSelfGuarantee: z.boolean(),
+    guarantors: z
+      .array(
+        z.object({
+          id: z.string(),
+          guarantorId: requiredStringSchemaEntry('Enter Guarantor ID.'),
+          guarantorName: requiredStringSchemaEntry('Enter Guarantor name.'),
+          guarantorContact: requiredStringSchemaEntry(
+            'Enter Guarantor contact.',
+          ).refine((val) => val.startsWith('0') && val.length === 10, {
+            message: 'Invalid Phone number provided.',
+          }),
+          amountGuaranteed: requiredNumberSchemaEntry(
+            'Enter amount guaranteed by the Guarantor.',
+          ),
+        }),
+      )
+      .optional(),
+  })
+  .superRefine(
+    (
+      { repaymentPeriod, maximumRepaymentPeriod, loanAmount, loanLimit },
+      ctx,
+    ) => {
+      if (repaymentPeriod > maximumRepaymentPeriod) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: `Repayment period cannot be greater than ${maximumRepaymentPeriod}.`,
+          path: ['repaymentPeriod'],
+        })
+      }
+      if (loanAmount > loanLimit) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: `Loan amount cannot be greater than your current loan limit - Ksh ${loanLimit}.`,
+          path: ['loanAmount'],
+        })
+      }
+    },
+  )
