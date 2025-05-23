29e9cc5bd80b8b1b17472d49c8564cc93c01e5ef
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-23 16:29:16 +0300

    feat: add loan details component

--- src/routes/_auth/loans_.$loanId.tsx
+++ src/routes/_auth/loans_.$loanId.tsx
@@ -1,11 +1,20 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { useDocumentTitle } from '@/hooks/use-title'
+import { loanQueryOptions } from '@/features/loans/services/query-options'
+import { LoanDetails } from '@/features/loans/components/loan-details'
 
 export const Route = createFileRoute('/_auth/loans_/$loanId')({
+  loader: ({ context, params: { loanId } }) =>
+    context.queryClient.ensureQueryData(loanQueryOptions(loanId)),
   component: RouteComponent,
 })
 
 function RouteComponent() {
   useDocumentTitle('Loan Details')
-  return <div>Hello "/_auth/loans_/$loanId"!</div>
+  const { loanId } = Route.useParams()
+  const {
+    data: { data: loan },
+  } = useSuspenseQuery(loanQueryOptions(loanId))
+  return <LoanDetails loan={loan} />
 }
