6fb1b78191d863086c2d408e08974e5483e2ea9b
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-26 09:52:06 +0300

    fix:change route to escape layout

--- src/routes/_auth/loans_.$loanId.statement.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_auth/loans_/$loanId/statement')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_auth/loans_/$loanId/statement"!</div>
-}
